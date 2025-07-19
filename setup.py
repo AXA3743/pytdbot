7e0fc59769df6ba76c86c1687b4a20ab9167879d
axa <109595100+AXA3743@users.noreply.github.com>
2025-07-19 15:26:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='pytdbot',
     version='0.1',
+    description='Simple tdlib wrapper for Telegram bot using python',
+    author='AXA3743',
     packages=find_packages(),
     install_requires=[],
+    python_requires='>=3.7',
 )
